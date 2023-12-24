732fe840d63256dbeb3be2a23b59297b3ae8f580
Kartik Yadav <ky5986390@gmail.com>
2023-12-24 22:03:28 +0530

    ep3

---
+++ App.js
@@ -0,0 +1,639 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import logo from "./public/assets/images/logo.jpg";
+  
+
+const data =  [
+    {
+    "info": {
+    "id": "570253",
+    "name": "Burger King",
+    "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+    "locality": "Jawaharnagar Colony",
+    "areaName": "Jawaharnagar Colony",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+    "Burgers",
+    "American"
+    ],
+    "avgRating": 4.3,
+    "parentId": "166",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "1K+",
+    "sla": {
+    "deliveryTime": 42,
+    "lastMileTravel": 4.9,
+    "serviceability": "SERVICEABLE",
+    "slaString": "42 mins",
+    "lastMileTravelString": "4.9 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-24 03:45:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "60% OFF",
+    "subHeader": "UPTO ₹120"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-98db66ec-f4f9-460e-82a4-2ed078245ca6"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/burger-king-jawaharnagar-colony-agra-570253",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "407809",
+    "name": "Pizza Hut",
+    "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+    "locality": "Idgah Colony",
+    "areaName": "Agra Cantt",
+    "costForTwo": "₹300 for two",
+    "cuisines": [
+    "Pizzas"
+    ],
+    "avgRating": 4.2,
+    "parentId": "721",
+    "avgRatingString": "4.2",
+    "totalRatingsString": "500+",
+    "sla": {
+    "deliveryTime": 39,
+    "lastMileTravel": 2.9,
+    "serviceability": "SERVICEABLE",
+    "slaString": "39 mins",
+    "lastMileTravelString": "2.9 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-24 04:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "60% OFF",
+    "subHeader": "UPTO ₹120"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-98db66ec-f4f9-460e-82a4-2ed078245ca6"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/pizza-hut-idgah-colony-agra-cantt-agra-407809",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "202097",
+    "name": "New Pizza King",
+    "cloudinaryImageId": "0ba64c8321dc0b54e01a0eaeb5973771",
+    "locality": "Saket Colony Road",
+    "areaName": "Shahganj",
+    "costForTwo": "₹300 for two",
+    "cuisines": [
+    "Pizzas",
+    "Italian",
+    "Burgers",
+    "Beverages"
+    ],
+    "avgRating": 4.3,
+    "parentId": "9833",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "5K+",
+    "sla": {
+    "deliveryTime": 46,
+    "lastMileTravel": 3,
+    "serviceability": "SERVICEABLE",
+    "slaString": "46 mins",
+    "lastMileTravelString": "3.0 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-24 04:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "aggregatedDiscountInfoV2": {},
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-98db66ec-f4f9-460e-82a4-2ed078245ca6"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/new-pizza-king-saket-colony-road-shahganj-agra-202097",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "74826",
+    "name": "Subway",
+    "cloudinaryImageId": "1ace5fa65eff3e1223feb696c956b38b",
+    "locality": "Khandari",
+    "areaName": "Church Road",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+    "Healthy Food",
+    "Salads",
+    "Snacks",
+    "Desserts",
+    "Beverages"
+    ],
+    "avgRating": 4.2,
+    "parentId": "2",
+    "avgRatingString": "4.2",
+    "totalRatingsString": "1K+",
+    "sla": {
+    "deliveryTime": 45,
+    "lastMileTravel": 3.7,
+    "serviceability": "SERVICEABLE",
+    "slaString": "45 mins",
+    "lastMileTravelString": "3.7 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-24 11:55:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "aggregatedDiscountInfoV2": {},
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-98db66ec-f4f9-460e-82a4-2ed078245ca6"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/subway-khandari-church-road-agra-74826",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "361094",
+    "name": "Sweet Truth - Cake and Desserts",
+    "cloudinaryImageId": "2bff7ef1c40b8a1d356fcf9d487324f9",
+    "locality": "Nehru Nagar",
+    "areaName": "Civil Lines",
+    "costForTwo": "₹450 for two",
+    "cuisines": [
+    "Snacks",
+    "Bakery",
+    "Desserts",
+    "Beverages"
+    ],
+    "avgRating": 4.4,
+    "parentId": "4444",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "500+",
+    "sla": {
+    "deliveryTime": 34,
+    "lastMileTravel": 4.6,
+    "serviceability": "SERVICEABLE",
+    "slaString": "34 mins",
+    "lastMileTravelString": "4.6 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-24 03:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "60% OFF",
+    "subHeader": "UPTO ₹110"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-98db66ec-f4f9-460e-82a4-2ed078245ca6"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/sweet-truth-cake-and-desserts-nehru-nagar-civil-lines-agra-361094",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "639642",
+    "name": "Biryani Blues",
+    "cloudinaryImageId": "so3xjrdjiihywm8hk9ig",
+    "locality": "Nehru Nagar",
+    "areaName": "Civil Lines",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+    "Biryani",
+    "Hyderabadi",
+    "Lucknowi",
+    "Kebabs",
+    "Desserts",
+    "Beverages"
+    ],
+    "avgRating": 4.4,
+    "parentId": "13813",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "500+",
+    "sla": {
+    "deliveryTime": 31,
+    "lastMileTravel": 4.6,
+    "serviceability": "SERVICEABLE",
+    "slaString": "31 mins",
+    "lastMileTravelString": "4.6 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-24 03:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "20% OFF",
+    "subHeader": "UPTO ₹50"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-98db66ec-f4f9-460e-82a4-2ed078245ca6"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/biryani-blues-nehru-nagar-civil-lines-agra-639642",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "656890",
+    "name": "Chopstick",
+    "cloudinaryImageId": "4b3a762840764d6197ed256b62803d5b",
+    "locality": "Azad Nagar",
+    "areaName": "Khandari",
+    "costForTwo": "₹300 for two",
+    "cuisines": [
+    "Snacks",
+    "Chinese",
+    "Fast Food"
+    ],
+    "avgRating": 3.5,
+    "parentId": "1783",
+    "avgRatingString": "3.5",
+    "totalRatingsString": "50+",
+    "sla": {
+    "deliveryTime": 48,
+    "lastMileTravel": 4.6,
+    "serviceability": "SERVICEABLE",
+    "slaString": "48 mins",
+    "lastMileTravelString": "4.6 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-24 04:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "30% OFF",
+    "subHeader": "UPTO ₹75"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-98db66ec-f4f9-460e-82a4-2ed078245ca6"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/chopstick-azad-nagar-khandari-agra-656890",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "info": {
+    "id": "361092",
+    "name": "Oven Story Pizza - Standout Toppings",
+    "cloudinaryImageId": "ab979bffbd658e74de650a15ca0092a3",
+    "locality": "Nehru Nagar",
+    "areaName": "Civil Lines",
+    "costForTwo": "₹400 for two",
+    "cuisines": [
+    "Pizzas",
+    "Pastas",
+    "Italian",
+    "Desserts",
+    "Beverages"
+    ],
+    "avgRating": 4.3,
+    "parentId": "3534",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "1K+",
+    "sla": {
+    "deliveryTime": 27,
+    "lastMileTravel": 4.6,
+    "serviceability": "SERVICEABLE",
+    "slaString": "23-31 mins",
+    "lastMileTravelString": "4.6 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2023-12-24 03:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "60% OFF",
+    "subHeader": "UPTO ₹120"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {}
+    },
+    "analytics": {
+    "context": "seo-data-98db66ec-f4f9-460e-82a4-2ed078245ca6"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/restaurants/oven-story-pizza-standout-toppings-nehru-nagar-civil-lines-agra-361092",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    }
+];
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo_container">
+                <img className="logo" src={logo} alt="logo"></img>
+            </div>
+            <div className="nav_items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+const RestaurantCard = (props) => {
+    const {
+        name,
+        cuisines,
+        costForTwo,
+        sla,
+        cloudinaryImageId
+    } = props.resData?.info;
+    return (
+        <div className="res_card">
+            <img className="res_logo" src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`} alt="logo"/>
+            <h3>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <h4>{costForTwo}</h4>
+            <h4>{sla.slaString}</h4>
+        </div>
+    );
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>            
+            <div className="res_container">
+                {
+                    data.map((restaurant)=>(
+                        <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
+                    ))
+                }
+            </div>
+        </div>
+    )
+};
+
+const App = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>);
