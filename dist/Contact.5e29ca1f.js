e3c2facb0b41c6f660e239c9a45dcace921b5794
Kartik Yadav <ky5986390@gmail.com>
2024-01-04 03:53:10 +0530

    build

---
+++ dist/Contact.5e29ca1f.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire164e;(0,e.register)("248A0",function(r,t){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>a,set:void 0,enumerable:!0,configurable:!0});var i=e("ayMG0"),a=()=>(0,i.jsx)("div",{children:(0,i.jsx)("h1",{children:"Contact"})})});
+//# sourceMappingURL=Contact.5e29ca1f.js.map
